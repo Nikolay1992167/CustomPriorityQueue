f560c19ed2d03a0d93f82c14ca511029c9f53f3b
Nikolay1992167 <wulik1081@mail.ru>
2024-08-21 14:54:06 +0300

    test: added README.md

--- src/main/java/by/mnp/util/Constants.java
+++ src/main/java/by/mnp/util/Constants.java
@@ -1,6 +1,6 @@
 package by.mnp.util;
 
-public class Constants {
+public final class Constants {
 
     public static final String CAPACITY_ERROR_MESSAGE = "Capacity must be > 0!";
     public static final String NOTIFICATION_NOT_NULL = "Can't store null elements!";
